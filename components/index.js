cd358697261dd93ecd821a084289719f71b30fa7
Got-crypto <pecpalmplay@gmail.com>
2023-06-08 21:51:29 +0200

    app set up; files and folder structure

---
+++ components/index.js
@@ -0,0 +1,13 @@
+import Sales from "./sales";
+import Footer from "./footer";
+import Feedbacks from "./feedbacks";
+import Categories from "./categories";
+import Hero from "./hero";
+
+export {
+    Sales, 
+    Footer,
+    Hero,
+    Feedbacks,
+    Categories
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import '../styles/Home.scss'
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
